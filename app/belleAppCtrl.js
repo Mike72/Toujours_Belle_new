094a6f217f2b1807c66a38fc22e9f13884139bce
Mike72 <mike.zoltan72@hotmail.com>
2017-11-29 21:44:09 +0200

    script loginCtrl, loginCtrl

--- app/belleAppCtrl.js
+++ app/belleAppCtrl.js
@@ -1,13 +1,25 @@
-belleApp.controller("belleAppCtrl", function($scope,$location){
-    function User(fname,lname,telephone,userName,password){
+belleApp.controller("belleAppCtrl", function ($scope, $location, $http) {
+    function User(fname, lname, telephone, userName, password) {
         this.fname = fname;
-        this.lname = lname; 
+        this.lname = lname;
         this.telephone = telephone;
         this.userName = userName;
-        this.password = password; 
+        this.password = password;
     }
-var user = null;
- $scope.users = [new User("Dora", "Hajdu","0525239029", "Dorika1", "123456")];
+    //var user = null;
+    // $scope.users = [new User("Dora", "Hajdu","0525239029", "Dorika1", "123456")];
 
- console.log(JSON.stringify($scope.users));
+    // console.log(JSON.stringify($scope.users));
+
+
+
+    $http.get("app/data/users.json").then(function (response) {
+        $scope.users = [];
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.users.push(new User(response.data[i]));
+        }
+
+        console.log(JSON.stringify($scope.users));
+
+    });
 });
---
+++ app/login/loginCtrl.js
@@ -0,0 +1,17 @@
+belleApp.controller("loginCtrl" ,function ($scope, $uibModalInstance, $http, $location, User){
+
+$scope.userName = "Mike";
+$scope.password = "Bubika";
+
+    $http.get("app/data/users.json").then(function (response) {
+        $scope.users = [];
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.users.push(new User(response.data[i]));
+        }
+
+        console.log(JSON.stringify($scope.users));
+
+    });
+
+
+});
