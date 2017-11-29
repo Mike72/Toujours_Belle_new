ccf6e22bfbdd8308ef3793a4113a61a400364d8c
Mike72 <mike.zoltan72@hotmail.com>
2017-11-29 14:27:08 +0200

    navbar-brand style belleAppCtrl.js update

--- app/belleAppCtrl.js
+++ app/belleAppCtrl.js
@@ -1,13 +1,13 @@
 belleApp.controller("belleAppCtrl", function($scope,$location){
-    function Guest(fname,lname,telephone,userName,password){
+    function User(fname,lname,telephone,userName,password){
         this.fname = fname;
         this.lname = lname; 
         this.telephone = telephone;
         this.userName = userName;
         this.password = password; 
     }
-var guest = null;
- $scope.guests = [new Guest("Dora", "Hajdu","0525239029", "Dorika1", "123456")];
+var user = null;
+ $scope.users = [new User("Dora", "Hajdu","0525239029", "Dorika1", "123456")];
 
- console.log(JSON.stringify($scope.guests));
+ console.log(JSON.stringify($scope.users));
 });
